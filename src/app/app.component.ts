703b001f376e53056f066786c9dcbb9880a1da6a
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-07-08 15:11:04 -0300

    cConsumindo API rest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
 import { PoMenuItem } from '@po-ui/ng-components';
+import { Cadastro } from './components/cnpj-list/cnpj-list.models';
+import { CadastroService } from './service/cadastro.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
 
-  constructor(private router:Router){
+
+
+
+
+export class AppComponent implements OnInit {
+  cadastro ={} as Cadastro;
+  cadastros: Cadastro[] | undefined;
+  CadastroService: any;
+
+  constructor(private router:Router, CadastroService:CadastroService){
 
   }
+  ngOnInit(): void {
 
+  }
   readonly menus: Array<PoMenuItem> = [
     { label: 'Home', action:()=>this.router.navigate(['']) },
     { label:'Consulta Cnpj', action:()=>this.router.navigate(['cnpj-list'])},
     { label:'Fornecedor',action:()=>this.router.navigate(['fornecedor-list'])}
-
-
-
   ];
 
+// chama o serviço GET
+  getCadastros() {
+    this.CadastroService.getCadastro().subscribe((cadastros: Cadastro[]) => {
+      this.cadastros = cadastros;
+    });
+  }
+
+  // deleta um CNPJ
+  deleteCNPJ(cadastro: Cadastro) {
+    this.CadastroService.deleteCNPJ(cadastro).subscribe(() => {
+      this.getCadastros();
+    });
+  }
+
+  // copia o CNPJ para ser editado.
+  editCar(cadastro: Cadastro) {
+    this.cadastro = { ...cadastro };
+  }
+
+  // limpa o formulario
+  cleanForm(form: NgForm) {
+    this.getCadastros();
+    form.resetForm();
+    this.cadastro = {} as Cadastro;
+  }
 
 }
 
