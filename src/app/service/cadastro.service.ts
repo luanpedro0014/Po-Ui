4a597f67c1c255b404993cca0d3e35800c941b2f
luanpedro0014 <42987466+luanpedro0014@users.noreply.github.com>
2021-07-08 15:14:51 -0300

    cConsumindoAPI

--- src/app/service/cadastro.service.ts
+++ src/app/service/cadastro.service.ts
@@ -1,17 +1,83 @@
-import { Inject, Injectable } from "@angular/core";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { Cadastro } from '../components/cnpj-list/cnpj-list.models';
 
-@Injectable()
-export class CadastroService{
-  elementAPiUrl ='http://localhost:3000/cadastro'
-constructor(private http:HttpClient){}
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
 
-getElements(): Observable<CadastroService[]>{
-return this.http.get<CadastroService[]>(this.elementAPiUrl);
+  url = 'https://brasilapi.com.br/api/cnpj/v1/10480616000160'; // api rest fake
 
+  // injetando o HttpClient
+  constructor(private httpClient: HttpClient) { }
+
+  // Headers
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
+
+  // Obtem todos os CNPJ
+  getCadastro(): Observable<Cadastro[]> {
+    return this.httpClient.get<Cadastro[]>(this.url)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  // Obtem um CNPJ pelo id
+  getCadastroById(CNPJ: number): Observable<Cadastro> {
+    return this.httpClient.get<Cadastro>(this.url + '/' + CNPJ)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+  }
+
+  // salva um CNPJ
+  saveCPNJ(cadastro: Cadastro): Observable<Cadastro> {
+    return this.httpClient.post<Cadastro>(this.url, JSON.stringify(cadastro), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      )
+    }
+
+  // atualiza um CNPJ
+  updateCadastro(cadastro: Cadastro): Observable<Cadastro> {
+    return this.httpClient.put<Cadastro>(this.url + '/' + cadastro.Id, JSON.stringify(cadastro), this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  // deleta um CNPJ
+  deleteCNPJ(cadastro: Cadastro) {
+    return this.httpClient.delete<Cadastro>(this.url + '/' + cadastro.CNPJ, this.httpOptions)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  // Manipulação de erros
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Erro ocorreu no lado do client
+      errorMessage = error.error.message;
+    } else {
+      // Erro ocorreu no lado do servidor
+      errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  };
 
 }
 
-
+function cadastro(cadastro: any): any {
 }
